174ce93b9c3c100d404b6b1694fb56954bbdfc71
5ylop <82076515+Huruk4u@users.noreply.github.com>
2024-02-14 21:39:05 +0900

    [Silver II] Title: N과 M (9), Time: 96 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver II/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver II/15663. N과 M （9）/N과 M （9）.py
@@ -5,15 +5,14 @@ input = sys.stdin.readline
 # 8!
 def backtracking(arr):
     if len(arr) == M:
-        # log(N!)
-        if tuple(arr) not in rtn:
-            rtn.add(tuple(arr))
+        print(*arr)
         return
-
+    prev = 0
     for i in range(N):
-        if is_used[i]:
+        if is_used[i] or prev == number[i]:
             continue
         is_used[i] = True
+        prev = number[i]
         backtracking(arr + [number[i]])
         is_used[i] = False
 
@@ -24,14 +23,12 @@ if __name__ == '__main__':
     N, M = map(int, input().strip().split())
     number = sorted(list(map(int, input().strip().split())))
     is_used = [False] * N
-    rtn = set()
 
+    prev = 0
     for i in range(N):
+        if prev == number[i]:
+            continue
         is_used[i] = True
+        prev = number[i]
         backtracking([number[i]])
         is_used[i] = False
-
-    rtn = sorted(rtn)
-    for r in rtn:
-        print(*r)
-
