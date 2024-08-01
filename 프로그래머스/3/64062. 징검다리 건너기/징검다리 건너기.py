f6b38a5cb20ec557239aeee222d6cb27a09e9227
5ylop <82076515+Huruk4u@users.noreply.github.com>
2024-08-01 19:54:04 +0900

    [level 3] Title: 징검다리 건너기, Time: 500.91 ms, Memory: 95.4 MB -BaekjoonHub

--- 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -1,21 +1,29 @@
 def solution(stones, k):
     answer = 0
-    sorted_stones = []
-    for i in range(len(stones)):
-        sorted_stones.append((stones[i], i))
-    sorted_stones.sort()
     
-    prev = [i-1 for i in range(len(stones) + 2)]
-    next = [i+1 for i in range(len(stones) + 2)]
-    
-    # O(N)
-    for num, idx in sorted_stones:
-        jump = next[idx] - prev[idx]
-        if jump > k:
-            answer = num
-            break
+    # pivot: 지나간 친구들의 수, pivot_list: 정렬된 돌의 숫자
+    pivot_list = sorted(list(set(stones)))
 
-        prev[next[idx]] = prev[idx]
-        next[prev[idx]] = next[idx]
+    prev_stone, number_to_idx = dict(), dict()
+    stones = [200001] + stones + [200001] # start + stones + end
+    # [inf, 2, 4, 5, 3, 2, 1, 4, 2, 5, 1, inf]
+    for i in range(1, len(stones) - 1):
+        if i != 0: prev_stone[i] = i-1
+        else: prev_stone[i] = 0
+        
+        if stones[i] not in number_to_idx: number_to_idx[stones[i]] = [i]
+        else: number_to_idx[stones[i]].append(i)
+        
+    jump = dict()
+    for i in range(len(stones)):
+        jump[i] = 1
+        
+    for pivot in pivot_list:
+        # if pivot = 1 -> idx = [5, 9]
+        for idx in number_to_idx[pivot]:
+            jump[prev_stone[idx]] += jump[idx]
+            prev_stone[idx + jump[idx]] = prev_stone[idx]
+            if jump[prev_stone[idx]] > k:
+                return pivot
     
     return answer
