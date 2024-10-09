ea499e32a4cd8d8ba964ea9bdd996118c6578aef
5ylop <82076515+Huruk4u@users.noreply.github.com>
2024-10-09 21:17:49 +0900

    [level 4] Title: 트리 트리오 중간값, Time: 1232.96 ms, Memory: 93.9 MB -BaekjoonHub

---
+++ 프로그래머스/4/68937. 트리 트리오 중간값/트리 트리오 중간값.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+
+def solution(N, edges):
+    
+    def bfs(queue):
+        vst = [-1] * (N+1)
+        vst[queue[0]] = 0
+        while queue:
+            curr = queue.popleft()
+            for next in graph[curr]:
+                if vst[next] != -1: continue
+                queue.append(next)
+                vst[next] = vst[curr] + 1
+                
+        return vst
+    
+    graph = [[] for _ in range(N+1)]
+    for u, v in edges:
+        graph[u].append(v)
+        graph[v].append(u)
+    
+    root, root_deg = 0, 0
+    for i in range(len(graph)):
+        if len(graph[i]) > root_deg:
+            root_deg = len(graph[i])
+            root = i
+            
+    dist = bfs(deque([root]))
+    leaf = dist.index(max(dist))
+    
+    dist = bfs(deque([leaf]))
+    leaf = dist.index(max(dist))
+    dist.sort(reverse = True)
+    if dist[0] == dist[1]:
+        return dist[0]
+    else:
+        dist = sorted(bfs(deque([leaf])), reverse=True)
+        answer = max(dist[1], dist[0]-1)
+        return answer
