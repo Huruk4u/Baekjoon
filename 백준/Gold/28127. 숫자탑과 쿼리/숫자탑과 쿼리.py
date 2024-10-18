c3783cabeb522d0479818d8790d447dceb7c9903
5ylop <82076515+Huruk4u@users.noreply.github.com>
2024-10-18 21:34:47 +0900

    [Gold V] Title: 숫자탑과 쿼리, Time: 552 ms, Memory: 120172 KB -BaekjoonHub

--- 백준/Gold/28127. 숫자탑과 쿼리/숫자탑과 쿼리.py
+++ 백준/Gold/28127. 숫자탑과 쿼리/숫자탑과 쿼리.py
@@ -5,13 +5,17 @@ input = sys.stdin.readline
 if __name__ == '__main__':
     for _ in range(int(input().strip())):
         a, d, x = map(int, input().strip().split())
-        left, right = 1, a
+        left, right = 1, x
         step = 1
-        while True:
-            if x <= right:
-                break
-            step += 1
-            left = right + 1
-            right += a + ((step - 1) * d)
+        while left < right:
+            mid = (right + left) // 2
 
-        print(step, x - left + 1)
+            total = (mid * (2 * a + (mid - 1) * d)) // 2
+            if x <= total:
+                right = mid
+                step = mid
+            else:
+                left = mid + 1
+
+        start = ((step - 1) * (2 * a + (step - 2) * d)) // 2 + 1
+        print(step, x - start + 1)
