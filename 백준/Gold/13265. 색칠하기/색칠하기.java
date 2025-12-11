8cc0d0b4e08df561356950300bd8538404b7dcce
Huruk4u <82076515+Huruk4u@users.noreply.github.com>
2025-12-12 00:56:49 +0900

    [Gold V] Title: 색칠하기, Time: 620 ms, Memory: 144244 KB -BaekjoonHub

---
+++ 백준/Gold/13265. 색칠하기/색칠하기.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    private static int N, M;
+
+    private static List<Integer>[] graph;
+
+    private static int[] color;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
+            st = new StringTokenizer(br.readLine().trim());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+
+            graph = new ArrayList[N+1];
+            for (int i = 0; i < N+1; i++) graph[i] = new ArrayList();
+
+            for (int i = 0; i < M; i++) {
+                st = new StringTokenizer(br.readLine().trim());
+                int u = Integer.parseInt(st.nextToken());
+                int v = Integer.parseInt(st.nextToken());
+                graph[u].add(v);
+                graph[v].add(u);
+            }
+
+            color = new int[N+1];
+            Arrays.fill(color, -1);
+
+            boolean answer = true;
+            for (int i = 1; i < N+1; i++) {
+                if (color[i] == -1) {
+                    answer = bfs(i) && answer;
+                }
+            }
+            System.out.println(answer? "possible" : "impossible");
+        }
+
+    }
+
+    private static boolean bfs(int start) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(start);
+
+        color[start] = 0;
+        while (!queue.isEmpty()) {
+            int curr = queue.remove();
+
+            for (int next : graph[curr]) {
+                if (color[next] == color[curr]) return false;
+                else { // color[next] == -1
+                    if (color[next] != -1) continue;
+                    queue.add(next);
+                    if (color[curr] == 0) color[next] = 1;
+                    else color[next] = 0;
+                }
+            }
+        }
+
+        return true;
+    }
+}
